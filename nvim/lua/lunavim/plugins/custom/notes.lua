8a67c374797f36ffbbe099fb9189aef0f5969868
NishikiChisato <hotaruakaridesu@gmail.com>
2026-01-10 07:53:21 +0800

    [lunavim] delete obsidian plugin

--- nvim/lua/lunavim/plugins/custom/notes.lua
+++ nvim/lua/lunavim/plugins/custom/notes.lua
@@ -20,25 +20,4 @@ return {
       },
     },
   },
-  {
-    "obsidian-nvim/obsidian.nvim",
-    lazy = true,
-    ft = "markdown",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = LunaVim.plugin_keymaps "obsidian.nvim",
-    opts = {
-      workspaces = {
-        {
-          name = "personal",
-          path = "~/workspace/DigitalGarden",
-        },
-      },
-      legacy_commands = false,
-      completion = { nvim_cmp = true, min_chars = 2 },
-      note_id_func = function(title)
-        return title
-      end,
-      ui = { enable = false },
-    },
-  },
 }
